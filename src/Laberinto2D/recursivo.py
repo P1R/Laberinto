54922d4232938a4a40df21b3088b294223b82bba
p1r0 <p1r0@insomnia247.nl>
2014-02-11 01:30:27 -0600

    version que saca todos los nodos explorados

--- src/Laberinto2D/recursivo.py
+++ src/Laberinto2D/recursivo.py
@@ -6,7 +6,10 @@ from time import sleep
 import MazeExit
 from pila import *
 
-N = int(raw_input("ingresa orden de la matriz:"))
+p1=Pila()
+
+#N = int(raw_input("ingresa orden de la matriz:"))
+N=15
 Y=[None]*N
 for i in range(N): 
 	Y[i]=[None]*N
@@ -21,7 +24,7 @@ for i in range(N):
 			Y[i][j]=0			
 Y[0][Arriba]=3
 			  
-def search(x, y, p1):
+def search(x, y):
 		if Y[x][y] == 3:
 			print 'Encontrado en %d,%d' % (y, x)
 			print 'con la siguiente pila:'
@@ -29,29 +32,45 @@ def search(x, y, p1):
 			return True
 		elif Y[x][y] == 1:
 			print 'Pared en %d,%d' % (y, x)
-			p1.desapilar()
 			return False
 		elif Y[x][y] == 2:
 			print 'Se visito %d,%d' % (y, x)
-			p1.apilar("[%d,%d]" % (y, x))
+			#p1.desapilar()
 			return False
 		print 'Visitando %d,%d' % (y, x)
-		# mark as visited
+		# marcamos como visitado
 		Y[x][y] = 2
-		if((x < len(Y)-1) or (y > 0) or(x > 0) or(y < len(Y)-1)):
+		i = 0
+		#seccion para apilar
+		if (x < len(Y)-1 and Y[x+1][y]==0):
+			i+=1
 			p1.apilar("[%d,%d]" % (y, x))
+		if (y > 0 and Y[x][y-1]==0):
+			i+=1
+			if i >= 2:
+				p1.apilar("[%d,%d]" % (y, x))
+		if (x > 0 and Y[x-1][y]==0):
+			i+=1
+			if i >= 2:
+				p1.apilar("[%d,%d]" % (y, x))
+		if (y < len(Y)-1 and Y[x][y+1]==0):
+			i+=1
+			if i >= 2:
+					p1.apilar("[%d,%d]" % (y, x))
+		if i == 0:
+			p1.desapilar()
+			
 		#sleep(0.01)
-		# explore neighbors clockwise starting by the one on the right
-		if((x < len(Y)-1 and search(x+1, y, p1))
-		or(y > 0 and search(x, y-1, p1))
-		or(x > 0 and search(x-1, y, p1))
-		or(y < len(Y)-1 and search(x, y+1,p1))):
+		#Explorar vecinos en orden del reloj
+		if((x < len(Y)-1 and search(x+1, y))
+		or(y > 0 and search(x, y-1))
+		or(x > 0 and search(x-1, y))
+		or(y < len(Y)-1 and search(x, y+1))):
 			return True
 		return False
 
 def main():
-	p1=Pila()
-	search(N-1, Abajo, p1)
+	search(N-1, Abajo)
 	pyplot.figure(figsize=(10, 5))
 	pyplot.imshow(Y, cmap=pyplot.cm.binary, interpolation='nearest')
 	pyplot.xticks([]), pyplot.yticks([])
