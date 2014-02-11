400277476dd069d1155b065d8c19f9d02e60f327
p1r0 <p1r0@insomnia247.nl>
2014-02-11 00:18:38 -0600

    agregando liberia de pila

--- src/Laberinto2D/recursivo.py
+++ src/Laberinto2D/recursivo.py
@@ -4,6 +4,7 @@ from random import randrange
 import matplotlib.pyplot as pyplot
 from time import sleep
 import MazeExit
+from pila import *
 
 N = int(raw_input("ingresa orden de la matriz:"))
 Y=[None]*N
@@ -19,32 +20,38 @@ for i in range(N):
 		else:
 			Y[i][j]=0			
 Y[0][Arriba]=3
-print Y
-	  
-def search(x, y):
+			  
+def search(x, y, p1):
 		if Y[x][y] == 3:
-			print 'Encontrado en %d,%d' % (x, y)
+			print 'Encontrado en %d,%d' % (y, x)
+			print 'con la siguiente pila:'
+			print p1.items
 			return True
 		elif Y[x][y] == 1:
-			print 'Pared en %d,%d' % (x, y)
+			print 'Pared en %d,%d' % (y, x)
+			p1.desapilar()
 			return False
 		elif Y[x][y] == 2:
-			print 'Se visito %d,%d' % (x, y)
+			print 'Se visito %d,%d' % (y, x)
+			p1.apilar("[%d,%d]" % (y, x))
 			return False
-		print 'Visitando %d,%d' % (x, y)
+		print 'Visitando %d,%d' % (y, x)
 		# mark as visited
 		Y[x][y] = 2
+		if((x < len(Y)-1) or (y > 0) or(x > 0) or(y < len(Y)-1)):
+			p1.apilar("[%d,%d]" % (y, x))
 		#sleep(0.01)
 		# explore neighbors clockwise starting by the one on the right
-		if ((x < len(Y)-1 and search(x+1, y))
-		or (y > 0 and search(x, y-1))
-		or (x > 0 and search(x-1, y))
-		or (y < len(Y)-1 and search(x, y+1))):
+		if((x < len(Y)-1 and search(x+1, y, p1))
+		or(y > 0 and search(x, y-1, p1))
+		or(x > 0 and search(x-1, y, p1))
+		or(y < len(Y)-1 and search(x, y+1,p1))):
 			return True
 		return False
 
 def main():
-	search(N-1, Abajo)
+	p1=Pila()
+	search(N-1, Abajo, p1)
 	pyplot.figure(figsize=(10, 5))
 	pyplot.imshow(Y, cmap=pyplot.cm.binary, interpolation='nearest')
 	pyplot.xticks([]), pyplot.yticks([])
